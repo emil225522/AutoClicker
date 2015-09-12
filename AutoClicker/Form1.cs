704c0bbda83f9f3882299cde8b8cccbd7924860e
emil225522 <ungothar@hotmail.com>
2015-09-12 10:25:59 +0200

    Initial commit.

---
+++ AutoClicker/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing;
+namespace AutoClicker
+{
+    public partial class AutoClicker : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        public AutoClicker()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            mouse_event(0x02 | 0x04, 0, 0, 0, 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+    }
+}
